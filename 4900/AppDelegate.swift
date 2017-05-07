af52721b57ecbdd8fcab3b6c834c9cb22cbda257
Laura Yang <laurayang@lauras-imac.hitronhub.home>
2017-05-06 17:16:50 -0700

    initial commit with static share posts storyboard

--- 4900/AppDelegate.swift
+++ 4900/AppDelegate.swift
@@ -15,6 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        let feedController =  FeedController(collectionViewLayout: UICollectionViewFlowLayout())
+        let navationConrtoller = UINavigationController(rootViewController: feedController)
+        window?.rootViewController = navationConrtoller
+        
+        UINavigationBar.appearance().barTintColor = UIColor(red: 217/255, green: 92/255, blue: 74/255, alpha: 1)
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        application.statusBarStyle = .lightContent
         // Override point for customization after application launch.
         return true
     }
